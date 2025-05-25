b43af910b13b203d5caaa1e2dfbea111a76d353c
Atan-Rut <atanatan190@gmail.com>
2025-05-25 16:21:08 +0800

    asdasdasdasda

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\File;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,6 +21,11 @@ public function boot()
 {
     if (app()->environment('production')) {
         URL::forceScheme('https');
+    }
+
+     $storageLink = public_path('storage');
+    if (!File::exists($storageLink)) {
+        File::link(storage_path('app/public'), $storageLink);
     }
 }
 }
